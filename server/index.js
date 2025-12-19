2f92d9b47d9cca9de30503981b9cd08f252fe334
cout-arya <aryaverma888@gmail.com>
2025-12-19 21:36:43 +0530

    Update catch-all route to Express 5 syntax (/:path*)

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ app.use('/api/auth', require('./routes/auth'));
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
 
-    app.get('(.*)', (req, res) => {
+    app.get('/:path*', (req, res) => {
         res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'));
     });
 } else {
